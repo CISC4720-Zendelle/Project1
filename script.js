4eb13ef37ff34b1e7356b7293c4cc53852779414
Zendelle Badiang <Zendelle@udel.edu>
2020-10-06 18:57:50 -0400

    debug

--- script.js
+++ script.js
@@ -9,130 +9,147 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 let gameData;
-var wordsarr = [];
-var count = 0;
-let myDB = firebase.database();
-let wordList = document.getElementById("pwords");
-var regexFilter = /[a-z]/g;
-var filler = "0";
+  var wordsarr = [];
+  var count = 0;
+  let myDB = firebase.database();
+  let wordList = document.getElementById("pwords");
+  var regexFilter = /[a-z]/g;
+  var filler = "0";
+ var correctCount = 0;
 
 myDB.ref("starting").on("value", ss =>{
-let status = ss.val();
-if (status == 1){
-  // $('button').attr("disabled", true);
-}
-
+  let status = ss.val();
+  if (status == 1){
+    // $('button').attr("disabled", true);
+  }
+  
 })
 
 myDB.ref("gameData").on("value",ss =>{
-let flag = ss.val();
-if(flag != null){
-  myDB.ref("gameData").child("rack").once("value", ss2 =>{
-     document.getElementById("STARTERS").innerText = ss2.val();
-  });
-  myDB.ref("gameData").child("words").once("value", ss3 =>{
-    let words = ss3.val();
-    console.log(words);
-    wordsarr= [];
-    count = 0;
-    for(word in words){
-      wordsarr.push(words[word]);//Add words to wordsarr
-      replacement = words[word].replace(regexFilter,filler);
-      addToList(wordList,replacement);
-      count++;
-    }
-  })
-}
+  let flag = ss.val();
+  if(flag != null){
+    myDB.ref("gameData").child("rack").once("value", ss2 =>{
+       document.getElementById("STARTERS").innerText = ss2.val();
+    });
+    myDB.ref("gameData").child("words").once("value", ss3 =>{
+      let words = ss3.val();
+      console.log(words);
+      wordsarr= [];
+      count = 0;
+      for(word in words){
+        wordsarr.push(words[word]);//Add words to wordsarr
+        replacement = words[word].replace(regexFilter,filler);
+        addToList(wordList,replacement);
+        count++;
+      }
+    })
+  }
 })
 
 //Fills Guess list
-myDB.ref("guesses").on("value", ss => {
-let guesses = ss.val();
-if (guesses != null){
-  for(guess in guesses){
-    addToList(document.getElementById("guesses"),guesses[guess]);
+myDB.ref("guesses").on("value", function(snapshot){
+  
+   snapshot.forEach(function (openSnapshot) {
+      console.log(openSnapshot.key); // The random key.
+      var val = openSnapshot.val();
+      console.log(val.guess);
+      console.log(val.match);
+     if(val.match == true){correctCount++};
+     addToList(document.getElementById("guesses"),val.guess, val.match);
+     
+  
+   });
+  
+  if(correctCount == count){
+    myDB.ref("starting").set(0);
+    document.getElementById("btn").disabled = false;
   }
-}
-})
+});
 
 //Clears list
 $('button').click(function() {
-$('ol').empty();
-$('ul').empty();
-myDB.ref("guesses").set(null);
+  $('ol').empty();
+  $('ul').empty();
+  correctCount = 0;
+  myDB.ref("guesses").set(null);
 });
 
 
 //Generate a new word
 function generateWord(){
-  myDB.ref("starting").set(1);
-  myDB.ref("dictionary").child("numstarters").once('value', ss =>{
-    let rackcount = parseInt(ss.val());
-    let randomrack= parseInt(Math.floor(rackcount*Math.random()));
+    myDB.ref("starting").set(1);
+    myDB.ref("starting").once("value", ss3 =>{
+      if(ss3.val() == 1){document.getElementById("btn").disabled = true;}
+    })
+    myDB.ref("dictionary").child("numstarters").once('value', ss =>{
+      let rackcount = parseInt(ss.val());
+      let randomrack= parseInt(Math.floor(rackcount*Math.random()));
 
-    myDB.ref("dictionary").child("starters").child(randomrack).once('value', ss2=>{
-      // document.getElementById("STARTERS").innerText = ss2.val();
-      let rack = ss2.val();
-      myDB.ref("dictionary").child("alphabetized").child(rack).once('value', ss3 =>{
-        let words = ss3.val();
-        gameData = {rack, words};
-        myDB.ref("gameData").set(gameData);
+      myDB.ref("dictionary").child("starters").child(randomrack).once('value', ss2=>{
+        // document.getElementById("STARTERS").innerText = ss2.val();
+        let rack = ss2.val();
+        myDB.ref("dictionary").child("alphabetized").child(rack).once('value', ss3 =>{
+          let words = ss3.val();
+          gameData = {rack, words};
+          myDB.ref("gameData").set(gameData);
+        });
       });
     });
-  });
-}
+  }
 
 
 
-function addToList(parent,input){
-var listElement = document.createElement("li");
-var text = document.createTextNode(input);
-listElement.appendChild(document.createTextNode(input));
-parent.appendChild(listElement);
+function addToList(parent,input,match){
+  var listElement = document.createElement("li");
+  var text = document.createTextNode(input);
+  listElement.appendChild(document.createTextNode(input));
+  if(match == true && parent.id == "guesses") {
+    listElement.style.backgroundColor = "green";
+  }
+  
+  parent.appendChild(listElement);
 }
 
 
 
 function fillArr(){
-console.log("arr called");
+  console.log("arr called");
 }
 
 
-//Take in user input
-var inputBox = document.getElementById("guess");
-inputBox.addEventListener("keyup", function(event){
-  if(event.keyCode === 13){
-    var guess = document.getElementById("guess");
-    // addToList(document.getElementById("guesses"),guess.value)
-    $("#guesses").empty();
-    console.log(checkGuess(guess.value));
-    if(checkGuess(guess.value) == 1){
-            myDB.ref("guesses").push({guess: guess.value, match: true});
-    }else{
-    myDB.ref("guesses").push({guess: guess.value, match: false});
+  //Take in user input
+  var inputBox = document.getElementById("guess");
+  inputBox.addEventListener("keyup", function(event){
+    if(event.code == "Enter"){
+      var guess = document.getElementById("guess");
+      // addToList(document.getElementById("guesses"),guess.value)
+      $("#guesses").empty();
+      console.log(checkGuess(guess.value));
+      if(checkGuess(guess.value) == 1){
+              myDB.ref("guesses").push({guess: guess.value, match: true});
+      }else{
+      myDB.ref("guesses").push({guess: guess.value, match: false});
+      }
+       guess.value = "";
     }
-     guess.value = "";
-  }
-})
+  })
 
 
 function checkGuess(input){
-let value = 0;
-
-myDB.ref("gameData/words").on("value", ss =>{
-  let words = ss.val();
-  for(word in words){
-    if(input == words[word]){
-      value = 1;
+  let value = 0;
+  
+  myDB.ref("gameData/words").on("value", ss =>{
+    let words = ss.val();
+    for(word in words){
+      if(input == words[word]){
+        value = 1;
+      }
     }
-  }
-});
-return value;
-
+  });
+  return value;
+  
 }
-
 
   
 
